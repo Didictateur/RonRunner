4ade0755a428ab38e36c9633b1b103cb90f34be6
Didictateur <adrien.decosse@gmail.com>
2026-02-09 13:41:07 +0100

    can create table

---
+++ ronrunner_1.0~beta1-1/usr/bin/table.py
@@ -0,0 +1,8 @@
+from player import Player
+
+class Table:
+    def __init__(self, p1: Player, p2: Player, p3: Player, p4: Player):
+        self.p1 = p1
+        self.p2 = p2
+        self.p3 = p3
+        self.p4 = p4
