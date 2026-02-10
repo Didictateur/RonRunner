b17e65a9553752dca7324c6d4d407b6799d6e8b4
Didictateur <adrien.decosse@gmail.com>
2026-02-10 10:37:58 +0100

    can delete player and table

--- ronrunner_1.0~beta1-1/usr/bin/table.py
+++ ronrunner_1.0~beta1-1/usr/bin/table.py
@@ -1,7 +1,9 @@
 from player import Player
 
 class Table:
-    def __init__(self, p1: Player, p2: Player, p3: Player, p4: Player, initalScore):
+    def __init__(self, id: int, p1: Player, p2: Player, p3: Player, p4: Player, initalScore: int):
+        self.id = id
+        
         self.p1 = p1
         self.p2 = p2
         self.p3 = p3
